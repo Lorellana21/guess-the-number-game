2c54e8c7ac9d0a6c81743503aeb226a0e031b2a7
Lorellana <lorellana.ordonez@gmail.com>
2021-11-16 13:23:40 +0100

    almost finished

--- js/main.js
+++ js/main.js
@@ -1,35 +1,43 @@
 'use strict';
 
-const mainElement = document.querySelector ('.js-main');
+const mainElement = document.querySelector ('.js-page');
 const titleElement = document.querySelector ('.js-title');
 const selectElement = document.querySelector ('.js-select');
 const buttonElement = document.querySelector ('.js-button');
 
 
-
-
-
-function handlerClickButton(event){
+function refreshTitle(){
 const selectedItem = selectElement.value;
 titleElement.innerHTML = selectedItem;
 }
 
-
-
-
-
-buttonElement.addEventListener("click", handlerClickButton);
-
-//generar numero aleatorio
-function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
-    mainElement.classList.remove("backgroundCity", "backgroundCountryside")
-    // if(numero aleatroio es par){
-        mainElement.classList.add("backgroundCity")
-    // } else {
-        mainElement.classList.add("backgroundCountryside");
-    }
-//   }
+function getRandomInt(){
+    const number =  Math.floor(Math.random() * 100);
+    return number;
+  }
   
 
-console.log(getRandomInt(100));
+const removeBackgroundClass = () => {
+mainElement.classList.remove("backgroundCity", "backgroundCountryside");
+}
+
+function setNewBackground(randomNumber){
+    if(randomNumber % 2 === 0){
+        mainElement.classList.add("backgroundCity");
+    } else { 
+        mainElement.classList.add("backgroundCountryside");
+    }
+}
+
+function refreshBackground(){
+    const number = getRandomInt();
+    removeBackgroundClass();
+    setNewBackground(number);
+
+}
+
+function handlerClickButton(){
+    refreshTitle();
+    refreshBackground();
+}
+buttonElement.addEventListener("click", handlerClickButton);
